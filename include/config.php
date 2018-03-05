a5682a940c59304116b52eb2944aea7c1a4408eb
aarzee <cregmi@abo.fi>
2018-03-05 20:42:24 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -3,17 +3,17 @@
     // FTP credentials - host: d3mill.net, user: project@d3mill.net, password: project@d3mill.net  
     
     //Database credentials
-    $servername = "d3mill.net";
-    $username = "d3mill_project";
-    $password = "admin@project";
+    $servername = "host";
+    $username = "user";
+    $password = "password";
 
     //Tables available to connect - users(SSN,Name,Email,DOB) , ... 
 
     try
     {
-	    $pdo = new PDO("mysql:host=$servername;dbname=d3mill_project", $username, $password);
+	    $pdo = new PDO("mysql:host=$servername;dbname=database_name", $username, $password);
 	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    //echo "Connected successfully". "</br>";
+	    echo "Connected successfully". "</br>";
     }
 
     catch(PDOException $e)
